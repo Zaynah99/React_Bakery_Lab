39fd706f70b84294b23be02724d04bbbf74831ad
Zaynah <zaynah.sadiq@hotmail.com>
2023-05-17 21:49:07 +0100

    added to cake form

--- src/cake/CakeForm.js
+++ src/cake/CakeForm.js
@@ -11,9 +11,27 @@ const handleFormSubmit = (e) => {
 }
 
 return (
+    <>
+    <h2>Add new a new cake!</h2>
+    <form onSubmit={handleFormSubmit}>
+    <input
+    type="text"
+    name="cakeName"
+    placeholder="Cake Name"
+    value={cakeName}
+    onChange={(e) => setCakeName(e.target.value)}/>
 
+    <input
+    type="text"
+    name="price"
+    placeholder="Price"
+    value={price}
+    onChange={(e) => setPrice(e.target.value)} />
 
-)
+    
+    </form>
+    </>
+);
 
 
 

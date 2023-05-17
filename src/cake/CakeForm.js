a646e1aa79337b5cefbae21cb4feab3b9978e004
Zaynah <zaynah.sadiq@hotmail.com>
2023-05-17 16:36:11 +0100

    added cake form

---
+++ src/cake/CakeForm.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+const cakeForm = ({cakes, setCakes}) => {
+    const[cakeName, setCakeName] = useState("");
+    const[price, setPrice] = useState();
+    const[rating, setRating] = useState();
+    const[ingredients, setIngredients] = useState("");
+
+const handleFormSubmit = (e) => {
+    e.preventDefault();
+}
+
+return (
+
+
+)
+
+
+
+
+
+
+
+
+}
